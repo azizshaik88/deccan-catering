b08d97c88e692031f6315c558cd0473852418300
aziz.shaik88 <aziz.shaik0228@gmail.com>
2016-05-14 21:24:43 -0500

    Starting from scratch

--- app.module.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-//var $ = require('jquery');
-
-
-var mainApp = angular.module('mainApp', ['ui.router']);
--- index.js
+++
@@ -1,6 +0,0 @@
-window.$ = window.jQuery = require('./assets/libs/node_modules/jquery');
- 
-var angular = require('./assets/libs/node_modules/angular'),
-    uirouter = require('./assets/libsnode_modules/angular-ui-router');
-
-var bootstrap = require('./assets/libs/node_modules/bootstrap');
