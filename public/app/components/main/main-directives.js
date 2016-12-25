e44a970e4c973581d79c6ea7ea11288829510b15
azizshaik88 <aziz.shaik0228@gmail.com>
2016-12-24 18:43:00 -0600

    Social media and slideshow

--- public/app/components/main/main-directives.js
+++ public/app/components/main/main-directives.js
@@ -10,3 +10,45 @@ app.directive('sideBarMenu', function () {
         controller: sideBarController
     };
 });
+
+
+
+app.directive('slideshow', function() {
+    
+    var slideshowController = function($scope) {
+          $scope.myInterval = 5000;
+        $scope.noWrapSlides = false;
+        $scope.active = 0;
+        
+          $scope.slides = [
+            {
+              image: '/img/biryani.jpg'
+            },
+            {
+              image: '/img/roasted_tandoori_chicken_closeup.jpg'
+            },
+            {
+              image: '/img/white_rice_and_chicken_curry.jpg'
+            },
+            {
+              image: '/img/lamb_biryani_big.jpg'
+            },  
+            {
+              image: '/img/yellow_rice_and_minced_meat.jpg'
+            },
+            {
+              image: '/img/baghare_baigan.jpg'
+            },
+            {
+              image: '/img/biryani_multiple.jpg'
+            },
+              
+          ];
+    };
+    
+    return {
+        restrict: 'EA',
+        controller: slideshowController
+    }
+    
+});
